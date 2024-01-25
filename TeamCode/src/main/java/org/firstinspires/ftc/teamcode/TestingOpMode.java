6cae2f13d1cb8efb53af29052d78d8b813e6d313
0xCUBE <94565160+0xCUB3@users.noreply.github.com>
2024-01-25 16:25:05 -0500

    Change orientation for Field Centric Movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingOpMode.java
@@ -19,16 +19,20 @@ public class TestingOpMode extends LinearOpMode {
 
         waitForStart();
 
-        while(opModeIsActive()) {
-            drive();
+        while (opModeIsActive()) {
+            dualStickDrive();
         }
+
+        telemetry.update();
 
     }
 
-    void drive() {
+    void singleStickDrive() {
         float y = -gamepad1.left_stick_y;
         float x = gamepad1.left_stick_x;
         float rx = gamepad1.right_stick_x;
+
+        telemetry.addData("y value", y);
 
         double scalar = (1 - 0.5 * gamepad1.right_trigger) / Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
 
@@ -36,5 +40,24 @@ public class TestingOpMode extends LinearOpMode {
         bot.leftFrontMotor.setPower((rx + y + x) * scalar);
         bot.rightRearMotor.setPower((-rx + y + x) * scalar);
         bot.leftRearMotor.setPower((rx + y - x) * scalar);
+    }
+
+    void dualStickDrive() {
+        float leftY = -gamepad1.left_stick_y;
+        float leftX = gamepad1.left_stick_x;
+        double scalar = 1 - 0.5 * gamepad1.right_trigger;
+
+        double leftScalar = scalar / Math.max(Math.abs(leftY) + Math.abs(leftX), 1);
+        bot.leftFrontMotor.setPower((leftY - leftX) * leftScalar);
+        bot.leftRearMotor.setPower((leftY + leftX) * leftScalar);
+
+        float rightY = -gamepad1.right_stick_y;
+        float rightX = gamepad1.right_stick_x;
+
+        double rightScalar = scalar / Math.max(Math.abs(rightY) + Math.abs(rightX), 1);
+        bot.leftFrontMotor.setPower((rightY - rightX) * rightScalar);
+        bot.leftRearMotor.setPower((rightY + rightX) * rightScalar);
+
+        telemetry.addData("left y value", leftY);
     }
 }
