2392333fd0de802d9deea37e213ed25a6eaaf793
0xCUBE <94565160+0xCUB3@users.noreply.github.com>
2024-01-25 16:34:03 -0500

    Fix directions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMovement.java
@@ -38,7 +38,7 @@ public class FieldCentricMovement extends LinearOpMode {
 
         // Retrieve the IMU from the hardware map
         IMU imu = hardwareMap.get(IMU.class, "imu");
-        // Adjust the orientation parameters to match the robot robot
+        // Adjust the orientation parameters to match the robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
                 RevHubOrientationOnRobot.UsbFacingDirection.UP));
@@ -49,7 +49,7 @@ public class FieldCentricMovement extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y;
+            double y = gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
             double speedVar = 1.25-gamepad1.right_trigger;
