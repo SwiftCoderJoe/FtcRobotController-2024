976996e22d17bd4c892295a5eeca2d3fad3ddaa0
Joe Cardenas <coderjoe05@gmail.com>
2024-02-14 12:40:01 -0500

    delete more comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/FieldCentricMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/FieldCentricMovement.java
@@ -43,24 +43,6 @@ public class FieldCentricMovement extends LinearOpMode {
 
         while (opModeIsActive()) {
             double speedVar = gamepad1.left_bumper ? .3 : 1;
-            // This button choice was made so that it is hard to hit on accident,
-            // it can be freely changed based on preference.
-            // The equivalent button is start on Xbox-style controllers
-//            if (gamepad1.options) {
-//                imu.resetYaw();
-//            }
-//
-//            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-//
-//            // Rotate the movement direction counter to the bot's rotation
-//            double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
-//            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-            // rotX = rotX * 1.1;  // Counteract imperfect strafing
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
 
             float y = -gamepad1.left_stick_y;
             float x = gamepad1.left_stick_x;
