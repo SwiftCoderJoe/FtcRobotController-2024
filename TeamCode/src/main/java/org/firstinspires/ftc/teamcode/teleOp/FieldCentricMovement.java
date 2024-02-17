33d9cfdafe46d88dd78aba9d680fb444fa04637b
Joe Cardenas <coderjoe05@gmail.com>
2024-02-17 08:09:31 -0500

    change abort key

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/FieldCentricMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/FieldCentricMovement.java
@@ -72,7 +72,7 @@ public class FieldCentricMovement extends LinearOpMode {
             if (gamepad1.y) {
                 bot.topOfSlide.setPosition(0.6);
                 // Drive robot slowly until RGB below 30
-                while (bot.colorSensor.red() < 30 || bot.colorSensor.green() < 30 || bot.colorSensor.blue() < 30 && !gamepad1.b) {
+                while (bot.colorSensor.red() < 30 || bot.colorSensor.green() < 30 || bot.colorSensor.blue() < 30 && !gamepad1.left_bumper) {
                     bot.rightFrontMotor.setPower(0.2);
                     bot.leftFrontMotor.setPower(0.2);
                     bot.rightRearMotor.setPower(0.2);
